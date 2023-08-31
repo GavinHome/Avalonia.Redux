0971111591f505e2e188f77a528a3b2129fc2923
yangxiaomin <yangxiaoming512@yeah.net>
2023-08-31 22:58:57 +0800

    [feat] update layout

--- samples/samples/Pages/Todos/Page/Page.cs
+++ samples/samples/Pages/Todos/Page/Page.cs
@@ -3,6 +3,7 @@ using samples.Pages.Todos.Report;
 using samples.Pages.Todos.Todo;
 using System.Linq;
 using Avalonia;
+using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Shapes;
 using Avalonia.Layout;
 using Avalonia.Media;
@@ -43,71 +44,90 @@ public partial class ToDoListPage : Page<PageState, Dictionary<string, dynamic>>
         {
             var todos = ctx.buildComponents();
             var report = ctx.buildComponent("report");
-            return new StackPanel
+            return new DockPanel
             {
+                [Grid.IsSharedSizeScopeProperty] = true,
                 Children =
                 {
-                    new Grid
+                    new StackPanel
                     {
-                        Margin = Thickness.Parse("8"),
-                        RowDefinitions = new RowDefinitions
-                        {
-                            new() { Height = GridLength.Star },
-                            new() { Height = GridLength.Auto }
-                        },
+                        [DockPanel.DockProperty] = Dock.Bottom,
+                        [DockPanel.ZIndexProperty] = 99,
                         Children =
                         {
-                            new ItemsControl
+                            new Border
                             {
-                                ItemsSource = todos,
-                            },
-                        }
-                    },
-                    new Border
-                    {
-                        Child = new StackPanel
-                        {
-                            Children =
-                            {
-                                new Border
+                                Padding = Thickness.Parse("8"),
+                                Child = new Grid
                                 {
-                                    [Grid.RowProperty] = 1,
-                                    Child = report
-                                },
-                                new Border
-                                {
-                                    [Grid.RowProperty] = 1,
-                                    HorizontalAlignment = HorizontalAlignment.Right,
-                                    VerticalAlignment = VerticalAlignment.Center,
-                                    Background = SolidColorBrush.Parse("#bbe9d3ff"),
-                                    Padding = new Thickness(0),
-                                    CornerRadius = new CornerRadius(15),
-                                    BoxShadow = new BoxShadows(new BoxShadow()
-                                        { OffsetX = 1, OffsetY = 5, Spread = 1, Blur = 8, Color = Colors.LightGray }),
-                                    Child = new Button()
+                                    Children =
                                     {
-                                        Background = new SolidColorBrush(Colors.Transparent),
-                                        CornerRadius = new CornerRadius(15),
-                                        Padding = new Thickness(0),
-                                        Height = 50, Width = 50,
-                                        BorderThickness = new Thickness(0),
-                                        Content = new Border
+                                        new Border
                                         {
-                                            Background = new SolidColorBrush(Colors.Transparent),
-                                            Padding = new Thickness(8, 5, 12, 8),
-                                            Child = new Path
-                                            {
-                                                Data = Geometry.Parse("M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"),
-                                                Fill = new SolidColorBrush(Colors.Black),
-                                                HorizontalAlignment = HorizontalAlignment.Center,
-                                                VerticalAlignment = VerticalAlignment.Center,
-                                            },
+                                            Child = report
                                         },
-                                        Command = ReactiveCommand.Create(() =>
-                                            dispatch(CounterActionCreator.onAddAction()))
+                                        new Border
+                                        {
+                                            Margin = Thickness.Parse("0 -20 10 8"),
+                                            HorizontalAlignment = HorizontalAlignment.Right,
+                                            VerticalAlignment = VerticalAlignment.Center,
+                                            Background = SolidColorBrush.Parse("#bbe9d3ff"),
+                                            Padding = new Thickness(0),
+                                            CornerRadius = new CornerRadius(15),
+                                            Child = new Button()
+                                            {
+                                                Background = SolidColorBrush.Parse("#bbe9d3ff"),
+                                                CornerRadius = new CornerRadius(15),
+                                                Padding = new Thickness(0),
+                                                Height = 50, Width = 50,
+                                                BorderThickness = new Thickness(0),
+                                                Content = new Border
+                                                {
+                                                    //Background = SolidColorBrush.Parse("#bbe9d3ff"),
+                                                    Padding = new Thickness(8, 5, 12, 8),
+                                                    Child = new Path
+                                                    {
+                                                        Data = Geometry.Parse(
+                                                            "M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"),
+                                                        Fill = new SolidColorBrush(Colors.Black),
+                                                        HorizontalAlignment = HorizontalAlignment.Center,
+                                                        VerticalAlignment = VerticalAlignment.Center,
+                                                    },
+                                                },
+                                                Command = ReactiveCommand.Create(() =>
+                                                    dispatch(CounterActionCreator.onAddAction()))
+                                            }
+                                        }
                                     }
                                 }
                             }
+                        }
+                    },
+                    new StackPanel
+                    {
+                        Children =
+                        {
+                            new Grid
+                            {
+                                Margin = Thickness.Parse("8"),
+                                RowDefinitions = new RowDefinitions
+                                {
+                                    new() { Height = GridLength.Star },
+                                    new() { Height = GridLength.Auto }
+                                },
+                                Children =
+                                {
+                                    new ScrollViewer
+                                    {
+                                        VerticalScrollBarVisibility = ScrollBarVisibility.Visible,
+                                        Content = new ItemsControl
+                                        {
+                                            
+                                            ItemsSource = todos,
+                                        },
+                                    }
+                                }
+                            },
                         }
                     }
                 }
@@ -127,13 +147,13 @@ public partial class ToDoListPage : Page<PageState, Dictionary<string, dynamic>>
                 isDone: true),
             new ToDoState(uniqueId: "2", title: "Hello Avalonia Redux",
                 desc: "Learn how to use Avalonia Redux in an avalonia app."),
-            // new ToDoState(uniqueId: "3", title: "Hello world", desc: "Learn how to program.", isDone: true),
-            // new ToDoState(uniqueId: "4", title: "Hello Avalonia", desc: "Learn how to build an avalonia app.",
-            //     isDone: true),
-            // new ToDoState(uniqueId: "5", title: "Hello Avalonia Redux",
-            //     desc: "Learn how to use Avalonia Redux in an avalonia app."),
-            // new ToDoState(uniqueId: "6", title: "Hello Avalonia Redux",
-            //     desc: "Learn how to use Avalonia Redux in an avalonia app.")
+            new ToDoState(uniqueId: "3", title: "Hello world", desc: "Learn how to program.", isDone: true),
+            new ToDoState(uniqueId: "4", title: "Hello Avalonia", desc: "Learn how to build an avalonia app.",
+                isDone: true),
+            new ToDoState(uniqueId: "5", title: "Hello Avalonia Redux",
+                desc: "Learn how to use Avalonia Redux in an avalonia app."),
+            new ToDoState(uniqueId: "6", title: "Hello Avalonia Redux",
+                desc: "Learn how to use Avalonia Redux in an avalonia app.")
         };
 
         ctx.Dispatch(new Action("initToDos", payload: initToDos));
