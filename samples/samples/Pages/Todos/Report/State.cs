b8fe66502e79ff912e9e920ebcc0c3060b494251
YangXiaoMin <yangxiaoming512@yeah.net>
2023-12-14 11:33:57 +0800

    [feat] the report view support xaml

--- samples/samples/Pages/Todos/Report/State.cs
+++ samples/samples/Pages/Todos/Report/State.cs
@@ -3,13 +3,21 @@ using ReactiveUI.Fody.Helpers;
 
 namespace samples.Pages.Todos.Report;
 
-internal class ReportState(int total, int done) : ReactiveObject
+public class ReportState : ReactiveObject
 {
     [Reactive]
-    public int Total { get; set; } = total;
+    public int Total { get; set; }
 
     [Reactive]
-    public int Done { get; set; } = done;
+    public int Done { get; set; }
+
+    public ReportState(int total, int done)
+    {
+        Total = total;
+        Done = done;
+    }
+
+    public ReportState() { }
 
     public override string ToString()
     {
