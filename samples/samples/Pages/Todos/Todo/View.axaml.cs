9a37df1097e7a4d36d7f9f07b049d2a1adfad858
YangXiaoMin <yangxiaoming512@yeah.net>
2023-12-14 16:29:55 +0800

    [feat] the pure view for todo

---
+++ samples/samples/Pages/Todos/Todo/View.axaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace samples.Pages.Todos.Todo;
+
+public partial class View : UserControl
+{
+    public static readonly StyledProperty<ICommand> OnEditCommandProperty =
+        AvaloniaProperty.Register<Button, ICommand>(
+            nameof(OnEditCommand));
+    
+    public static readonly StyledProperty<ICommand> OnRemoveCommandProperty =
+        AvaloniaProperty.Register<Button, ICommand>(
+            nameof(OnRemoveCommand));
+    
+    public static readonly StyledProperty<ICommand> OnDoneCommandProperty =
+        AvaloniaProperty.Register<Button, ICommand>(
+            nameof(OnDoneCommand));
+    
+    public View()
+    {
+        InitializeComponent();
+    }
+
+    public ICommand? OnRemoveCommand 
+    {
+        get => GetValue(OnRemoveCommandProperty);
+        set => SetValue(OnRemoveCommandProperty!, value);
+    }
+    public ICommand? OnDoneCommand 
+    {
+        get => GetValue(OnDoneCommandProperty);
+        set => SetValue(OnDoneCommandProperty!, value);
+    }
+    public ICommand? OnEditCommand 
+    {
+        get => GetValue(OnEditCommandProperty);
+        set => SetValue(OnEditCommandProperty!, value);
+    }
+
+    public IValueConverter BoolToBrushConverter { get; } =
+        new FuncValueConverter<bool, IBrush>(value => new SolidColorBrush(value ? Colors.Green : Colors.Red));
+}
+
+// public class BoolToColorConverter: IValueConverter
+// {
+//
+//     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+//     {
+//         if (value is bool and true)
+//         {
+//             return Brushes.Green;
+//         }
+//         
+//         return Brushes.Red;
+//     }
+//
+//     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+//     {
+//         return null;
+//     }
+// }
